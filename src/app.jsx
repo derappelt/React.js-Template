25062cd2bdb5228e875955a525f7845c6c5ad775
Daniel Crump <d.crump@taenzer.me>
2016-10-17 13:28:08 +0200

    added redux

--- src/app.jsx
+++ src/app.jsx
@@ -3,7 +3,13 @@ import ReactDOM from 'react-dom';
 import { Router, hashHistory } from 'react-router';
 import routes from './config/routes';
 
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
+let store = createStore(todoApp);
+
+
 ReactDOM.render(
-  <Router history={hashHistory}>{routes}</Router>,
+  <Router history={hashHistory} store={store}>{routes}</Router>,
   document.getElementById('app')
 )
